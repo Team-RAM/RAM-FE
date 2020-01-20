3864ebaf8f03ecef2acf7bb73a0e521d982cf336
Martmists <martmists@gmail.com>
2020-01-20 02:34:02 +0100

    add coww alpha and stable beeresourceful build

--- .minecraft/config/datapacks/custom_stuff/data/ram_fe/tweakers/conflicts.js
+++ .minecraft/config/datapacks/custom_stuff/data/ram_fe/tweakers/conflicts.js
@@ -6,3 +6,9 @@ var TRTweaker = libcd.require("techreborn.TRTweaker")
 RecipeTweaker.removeRecipe("soulshards:vile_dust")
 RecipeTweaker.removeRecipe("retroexchange:transmution_id_45")
 TRTweaker.addGrinder(["minecraft:soul_sand@1"], ["soulshards:vile_dust@2"], 2, 200)
+
+// Leftover dev recipes
+RecipeTweaker.removeRecipe("coww:cauldron/test_cauldron_recipe")
+RecipeTweaker.removeRecipe("coww:cauldron/test_cauldron_recipe_1")
+RecipeTweaker.removeRecipe("coww:cauldron/test_cauldron_recipe_mirrored")
+RecipeTweaker.removeRecipe("coww:cauldron/test_cauldron_recipe_potion")
